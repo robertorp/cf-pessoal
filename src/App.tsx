cbf219e567ca13a8bd1621da10dc7f1a24a802f8
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-08-17 21:35:39 -0300

    Criado dashboard é summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global";
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <>
       <Header></Header>
+      <Dashboard></Dashboard>
       <GlobalStyle></GlobalStyle>
     </>
-  ); 
+  );
 }
