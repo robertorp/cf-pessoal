987497b4ab54dece549a0799d7d2be37f0fcfb75
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-08-20 20:35:51 -0300

    Criado um componente para o Modal , Modal sendo feito para a criação de uma nova Transação

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 import Modal from 'react-modal';
 import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
 
@@ -25,12 +26,10 @@ export function App() {
       ></Header>
       <Dashboard></Dashboard>
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      ></NewTransactionModal>
 
       <GlobalStyle></GlobalStyle>
     </>
