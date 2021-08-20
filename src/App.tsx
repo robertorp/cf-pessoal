a0dd90da49b8bda441bc291c09f9cfebe8cb43df
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-08-20 20:25:07 -0300

    Adicionado Modal para Criação de Nova Transação, apenas a Modal foi Adicionada

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import Modal from 'react-modal';
+import { useState } from 'react';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header></Header>
+      <Header
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      ></Header>
       <Dashboard></Dashboard>
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle></GlobalStyle>
     </>
   );
